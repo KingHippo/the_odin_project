ec13efb76cc25c0168debb9947756882a05e76e9
Don Price <askdonno@gmail.com>
2015-06-06 23:00:25 -0400

    solve sum with while loop

--- learn_ruby/02_calculator/calculator.rb
+++ learn_ruby/02_calculator/calculator.rb
@@ -1,32 +1,17 @@
 def add(x,y)
-    x+y
+  x + y
 end
 
 def subtract(x,y)
-    x-y
+  x - y
 end
 
-def sum(numbers)
-    sum = 0
-    numbers.each { |i| sum += i}
-    sum
-end
-
-def multiply(multiple)
-    num = 1
-    multiple.each { |i| num *= i}
-    num
-end
-
-def power(x,y)
-       x**y
-end
-
-def factorial(x)
-    if x < 1
-        return 1
-    else
-        return x * factorial(x - 1)
-    end
-
+def sum(num)
+  total = 0
+  count = 0
+  while count < num.length
+    total += num[count]
+    count += 1
+  end
+  return total
 end
