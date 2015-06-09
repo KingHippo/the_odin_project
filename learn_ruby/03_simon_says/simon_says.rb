8f67f063cb494aacb623ab569d52f7ecb58ba849
Don Price <askdonno@gmail.com>
2015-06-09 00:15:26 -0400

    remove unnecessary tab spaces

--- learn_ruby/03_simon_says/simon_says.rb
+++ learn_ruby/03_simon_says/simon_says.rb
@@ -1,15 +1,19 @@
 def echo(str)
-    str
+  str
 end
 
 def shout(str)
-    str.upcase
+  str.upcase
 end
 
 def repeat(str, num=2)
-    ([str] * num).join(' ')
+  ([str] * num).join(' ')
 end
 
 def start_of_word(str, num)
-    str[0..num-1]
+  str[0..num-1]
+end
+
+def first_word
+  
 end
