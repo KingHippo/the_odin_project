8ce54dc8deb73f9a16daedb8cd8c393396b3ec59
Don Price <askdonno@gmail.com>
2015-06-22 02:04:52 -0400

    translate method now passes all tests

--- learn_ruby/04_pig_latin/pig_latin.rb
+++ learn_ruby/04_pig_latin/pig_latin.rb
@@ -1,12 +1,18 @@
-def translate (string)
+def translate(str)
+  vowels = ["a","e","i","o","u"]
+  words = str.split(" ")
 
-  vowel = ['a', 'e', 'i', 'o', 'u']
-
-  words = string.split(' ').each do |word|
-    if vowel.include?(word[0])
-      word = word + "ay"
+  words.map! do |word|
+    until vowels.include? word[0] do
+      if word[0..1] == "qu"
+        word = word[2..-1] + word[0..1]
+      elsif word[1..2] == "qu"
+        word = word[3..-1] + word[0..2]
+      else
+        word = word[1..-1] + word[0]
+      end
     end
-    return word
+    word = word + "ay"
   end
-
+  words.join(" ")
 end
