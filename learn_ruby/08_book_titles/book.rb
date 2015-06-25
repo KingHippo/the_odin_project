c9e0256c2c62268122b8dbc645d2349be726a99b
Don Price <askdonno@gmail.com>
2015-06-25 02:35:51 -0400

    refactor code for readability

--- learn_ruby/08_book_titles/book.rb
+++ learn_ruby/08_book_titles/book.rb
@@ -1,12 +1,10 @@
 class Book
   def title=(str)
+
     words = str.split(" ")
     lil = ["a", "an", "and", "in", "the", "of"]
-    
+
     words.map! do |word|
-
-      
-
       unless lil.include? word
         word.capitalize
       else
