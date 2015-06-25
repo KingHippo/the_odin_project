f29f2dbd9a15cb89092de414283127fdaa2eed0b
Don Price <askdonno@gmail.com>
2015-06-25 02:32:28 -0400

    title function always capitalizes first word

--- learn_ruby/08_book_titles/book.rb
+++ learn_ruby/08_book_titles/book.rb
@@ -1,15 +1,18 @@
 class Book
   def title=(str)
     words = str.split(" ")
-    lil = ["a", "an", "and", "in", "the", "of"]
 
     words.map! do |word|
-      unless lil.include? word || word == "the"
-        word.capitalize!
+
+      lil = ["a", "an", "and", "in", "the", "of"]
+
+      unless lil.include? word
+        word.capitalize
       else
         word
       end
     end
+      words[0].capitalize!
       @str = words.join(" ")
   end
 
@@ -17,3 +20,4 @@ class Book
     @str
   end
 end
+
