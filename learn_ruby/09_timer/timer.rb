5be14d34715af2cc5c279e61013fc9f708bac831
Don Price <askdonno@gmail.com>
2015-06-25 21:43:39 -0400

    initial commit

---
+++ learn_ruby/09_timer/timer.rb
@@ -0,0 +1,22 @@
+class Timer
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    time_format(@seconds)
+  end
+
+  def time_format(seconds)
+    time = seconds
+    format = [0,":",0,":",0]
+
+    if time > 60
+      hour = time / 60
+      minute = time % 60
+    end
+  end
+
+end
