e124245a912d187b36ce336d5d2aded235381e2e
Don Price <askdonno@gmail.com>
2015-06-29 00:49:30 -0400

    use ternary operator instead of if statement

--- learn_ruby/09_timer/timer.rb
+++ learn_ruby/09_timer/timer.rb
@@ -1,4 +1,5 @@
 class Timer
+
   attr_accessor :seconds
 
   def initialize
@@ -6,21 +7,14 @@ class Timer
   end
 
   def time_string
-    time_format(@seconds)
+    hour    = (@seconds/3600)
+    minutes = (@seconds/60)%60
+    seconds = (@seconds%60)
+
+    padded(hour).to_s + ":" + padded(minutes).to_s + ":" + padded(seconds).to_s
   end
 
-  def time_format(seconds)
-    time = seconds
-
-    if time > 60
-      hour  = time / 60
-      minute = time % 60
-    else
-      second = time
-    end
-
-    time_format = hour.to_s + ": " + minute.to_s + ": " + second.to_s
-    return time_format
+  def padded(num)
+    num < 10 ? "0" + num.to_s : num.to_s
   end
-
 end
