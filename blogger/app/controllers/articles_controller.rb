e28347d1bab40d350bec9dca0d5d3bafb7ca641e
Don Price <askdonno@gmail.com>
2015-07-03 20:22:01 -0400

    Implement destroy method and delete link

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
   def index
     @articles = Article.all
@@ -9,9 +10,15 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:article][:title]
+    @article = Article.new(article_params)
     @article.save
+
     redirect_to article_path(@article)
   end
+
+  def destroy
+    @article = Article.destroy(params[:id])
+    redirect_to articles_path(@articles)
+  end
+
 end
