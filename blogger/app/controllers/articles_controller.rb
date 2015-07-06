21dec09cbbd36204afd1f9cc07aafe0f6e975899
Don Price <askdonno@gmail.com>
2015-07-06 01:58:47 -0400

    Implement comments

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -5,6 +5,12 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
+  end
+
   def new
     @article = Article.new
   end
