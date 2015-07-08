09bda5fdc9814f35fc4ab695ce294cbc73d88487
Don Price <askdonno@gmail.com>
2015-07-08 01:49:02 -0400

    implement tagging

--- blogger/db/migrate/20150702224621_create_articles.rb
+++ blogger/db/migrate/20150702224621_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string    :title
       t.text      :body
+      
       t.timestamps null: false
     end
   end
