882ccadd3049c035690902cf7ed824ebfef24817
Don Price <askdonno@gmail.com>
2015-07-20 00:53:01 -0400

    convert each ASCII val to char and concatinate

--- caesar_cipher/cipher.rb
+++ caesar_cipher/cipher.rb
@@ -1,16 +1,23 @@
 def casear_cipher(string, shift)
+    # empty string for encrypted message
     cipher_text = ""
+    # convert each character to ASCII byte code
     string.each_byte {|val|
+      # increment ASCII value according to shift value
       shift.times {
+        # retains a-z values
         if(val == 122)
           val = 97
+        # retains A-Z values
         elsif(val == 90)
           val = 65
         end
+        # increment val by 1
         val += 1
       }
-      puts val.chr
+      cipher_text += val.chr
     }
+    puts cipher_text
 end
 
-casear_cipher("azAZbcd", 5)
+casear_cipher("What a string!", 5)
