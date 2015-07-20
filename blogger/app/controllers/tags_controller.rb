62ce342c120864748c3656f50cd280058447834c
Don Price <askdonno@gmail.com>
2015-07-19 21:39:54 -0400

    clean up

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -1,8 +1,19 @@
 class TagsController < ApplicationController
-
   before_filter :require_login, except: [:create]
+  
+  def index
+    @tag = Tag.all
+  end
+
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+  	@tag = Tag.find(params[:id])
+  	@tag.destroy
+  	flash.notice = "Tag '#{@tag.name}' Deleted!"
+  	redirect_to tags_path
   end
 
 end
