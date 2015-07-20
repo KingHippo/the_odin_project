045251ddb451e2650d2d4e14eaa7bd31dbe6a41f
Don Price <askdonno@gmail.com>
2015-07-19 23:44:47 -0400

    create nested block with itteration to change bytes

--- caesar_cipher/cipher.rb
+++ caesar_cipher/cipher.rb
@@ -1,5 +1,16 @@
 def casear_cipher(string, shift)
-    string.each_byte {|val| puts val.ord}
+    cipher_text = ""
+    string.each_byte {|val|
+      shift.times {
+        if(val == 122)
+          val = 97
+        elsif(val == 90)
+          val = 65
+        end
+        val += 1
+      }
+      puts val.chr
+    }
 end
 
-casear_cipher("hello", 5)
+casear_cipher("azAZbcd", 5)
